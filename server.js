23074b4728ae060a5277ef7be0e183a7eb4b9625
linaG <linagiradlot@hotmail.com>
2024-08-08 21:06:58 -0500

    tercer  commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 app.use('/api/auth', require('./routes/auth'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
