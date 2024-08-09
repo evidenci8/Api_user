808a78a094a64cfbd756f6d1e6e5b51b86ab440c
linaG <linagiradlot@hotmail.com>
2024-08-08 23:16:01 -0500

    trece  commit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,18 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+/*const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql'
+});*/
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  dialectOptions: {
+    connectTimeout: 60000 // 60 segundos
+  }
 });
+
 
 const connectDB = async () => {
   try {
