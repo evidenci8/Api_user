14615b133b101eb4877a7f5994cb3ea4f0137188
linaG <linagiradlot@hotmail.com>
2024-08-10 08:31:51 -0500

    quince  commit

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const { connectDB } = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-
+const path = require('path');
 const app = express();
 
 connectDB();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use('/api/auth', require('./routes/auth'));
 
